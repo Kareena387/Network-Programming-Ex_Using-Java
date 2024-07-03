e35a435e1d9039515602f78faf148ad9e672bbe5
Ka_Ree_Na <angewills387@gmail.com>
2024-07-03 21:59:16 +0545

    fetch all HTTP header Fields using URLConnection

--- src/FethHttpHeader.java
+++ src/FethHttpHeader.java
@@ -1,3 +1,4 @@
+//Lab: Write a java program to fetch all HTTP header Fields using URLConnection
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -8,15 +9,15 @@ public class FethHttpHeader {
         @SuppressWarnings("deprecation")
         URL u = new URL("https://monkeytype.com/");
         URLConnection uc = u.openConnection();
+
         Map<String,List<String>> header = uc.getHeaderFields();
+        
         for(Map.Entry<String, List<String>> mp1 : header.entrySet())
         {
             System.out.println(mp1.getKey() +":");
             System.out.println(mp1.getValue().toString());
         }
 
-        
     }
-
-    
+   
 }
